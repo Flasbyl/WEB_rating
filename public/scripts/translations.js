281486b3989cb39a0627144d5f449d5da95c80c2
Flasbyl <72549725+Flasbyl@users.noreply.github.com>
2024-11-27 12:48:22 +0100

    translation table added
    for workload and grade

---
+++ public/scripts/translations.js
@@ -0,0 +1,18 @@
+// Global grade and workload translations
+window.gradeTranslations = {
+    "6.0": "A",
+    "5.5": "B",
+    "5.0": "C",
+    "4.5": "D",
+    "4.0": "E",
+    "3.5": "Failed",
+  };
+  
+  window.workloadTranslations = {
+    "1": "Walk in the Park",
+    "2": "Manageable",
+    "3": "Moderate Effort",
+    "4": "Challenging",
+    "5": "Pain and Suffering",
+  };
+  
